2955e0b7fe3d3ca861e46710a9e84490346ba02b
Bishwarup Paul <diganta.biz@gmail.com>
2020-08-28 19:49:32 +0530

    Updating the code and readme

--- Flickr_data_download.R
+++ Flickr_data_download.R
@@ -5,7 +5,7 @@ library(FlickrAPI)
 # Specify your api_key 
 api_key <- "<API_KEY>"
 
-# Specify search keyword, locations, date
+# Specify search keyword, enable location, and other details
 keyword <- "<KEYWORD>"
 perpage <- "250"
 format <- "rest"
@@ -47,17 +47,21 @@ for(i in c(1:total_pages)){
 
     id<-xpathSApply(parse_data,"//photo",xmlGetAttr,"id")
 
+    #Save results in a dataframe
     tmp_df <- data.frame(id, stringsAsFactors=F)
     tmp_df$page <- i
     final_list<-rbind(final_list,tmp_df)
 }
 
+# Create empty vectors for data
 lats <- rep(NA, nrow(final_list))
 lons <- rep(NA, nrow(final_list))
 urls <- rep(NA, nrow(final_list))
 dates <- rep(NA, nrow(final_list))
 places <- rep(NA, nrow(final_list))
 
+# Populate the empty vectors with information
+# Try function is used in the loop to handle error due to missing data
 for(i in 1:nrow(final_list)){
     try({
         loc <- getPhotoInfo(api_key, final_list$id[i], output = "Location")
@@ -71,12 +75,14 @@ for(i in 1:nrow(final_list)){
     }, silent=T)
 }
 
+# Convert vectors into dataframes
 idDF <- as.data.frame(final_list$id)
 latDF <- as.data.frame(lats)
 lonDF <- as.data.frame(lons)
 urlDF <- as.data.frame(urls)
 placeDF <- as.data.frame(places)
 
+# Remove timestamp from date
 dateL <- strsplit(dates, " ")
 dateDF <- data.frame(rep(NA, length(dateL)))
 colnames(dateDF) <- "observation_date"
@@ -84,10 +90,13 @@ for(i in 1:length(dateL)){
     dateDF[i,] <- dateL[[i]][1]
 }
 
+# Collate all data in a dataframe
 data_collated <- cbind(idDF, dateDF, placeDF, lonDF, latDF, urlDF)
 colnames(data_collated) <- c("photo_id", "observation_date", "location", 
                             "longitude", "latitude", "link")
 
+# Remove missing values (points which do not have a link to a photo)
 data_NAremoved <- subset(data_collated, link != "NA")
 
+# Write the final dataframe into CSV file
 write.csv(data_NAremoved, paste(keyword, ".csv", sep=""), row.names=F)
--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This work was inspired by the repository [Flickr-API](https://github.com/Frances
 The R code file lets you search Flickr using any keyword, and download details of the photos obtained in the search result.
 
 You can obtain the date when the photo was taken, location, latitude, longitude and a link for the file, which are saved in a CSV file. The name of the CSV file is set to be the same as the keyword you use to search.
+
+Requires a Flickr API key, which you can obtain from [here](https://www.flickr.com/services/api/).
